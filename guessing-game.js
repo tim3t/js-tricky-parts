4a686b5dc22abcba3bc8a6e30f80f617f3d955ea
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2023-01-08 19:38:38 -0500

    Guessing Game done

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,18 @@
 function guessingGame() {
+	const SECRET_NUM = Math.floor(Math.random() * 100);
+	let gameOver = false;
+	let guesses = 0;
 
+	return function guess(num) {
+		if (gameOver) return 'You already won! Start a new game to keep playing!';
+		guesses++;
+		if (num === SECRET_NUM) {
+			gameOver = true;
+			return `You got it! It took you ${guesses} tries to find ${SECRET_NUM}`;
+		}
+		if (num < SECRET_NUM) return `${num} is too low, try again!`;
+		if (num > SECRET_NUM) return `${num} is too high, try again!`;
+	};
 }
 
 module.exports = { guessingGame };
