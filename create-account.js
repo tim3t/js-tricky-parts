286e6fb0217c4fb146b3dedd52a30660e7877aad
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2023-01-08 21:18:17 -0500

    Completed create-account exercise

--- create-account.js
+++ create-account.js
@@ -1,5 +1,26 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount = 0) {
+	return {
+		checkBalance(inputPin) {
+			if (inputPin !== pin) return 'Invalid PIN, please try again';
+			return `$${amount}`;
+		},
+		deposit(inputPin, depositAmount) {
+			if (inputPin !== pin) return 'Invalid PIN, please try again';
+			amount += depositAmount;
+			return `Deposited $${depositAmount}. Balance: $${amount}.`;
+		},
+		withdraw(inputPin, withdrawAmount) {
+			if (inputPin !== pin) return 'Invalid PIN, please try again';
+			if (withdrawAmount > amount) return 'Insufficient funds for withdraw. Transaction cancelled.';
+			amount -= withdrawAmount;
+			return `Withdraw of $${withdrawAmount} successful. Balance: ${amount}`;
+		},
+		changePin(oldPin, newPin) {
+			if (oldPin !== pin) return 'Invalid PIN';
+			pin = newPin;
+			return 'PIN succesfully changed';
+		}
+	};
 }
 
 module.exports = { createAccount };
